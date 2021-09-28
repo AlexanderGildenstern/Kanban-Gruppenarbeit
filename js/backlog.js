7b240efb8289e6110c9f1ea5302214572c27b2da
Niklas Zeiler <zeiler.niklas@outlook.de>
2021-09-28 20:49:24 +0200

    update backlog

--- js/backlog.js
+++ js/backlog.js
@@ -1,5 +1,6 @@
-function initBacklog() {
-    init();
+async function initBacklog() {
+    await init();
+    updateBacklog();
 }
 
 /**
@@ -10,15 +11,18 @@ function updateBacklog() {
     for (let i = 0; i < tasks.length; i++) {
         document.getElementById('generatedBacklog').innerHTML += generateBacklog(i);
 
-        // document.getElementById(`userImage${i}`).src = users[i]['profil_image'];
-        // document.getElementById(`backlogUser${i}`).innerHTML = users[i]['first_name'] + ' ' + users[i]['last_name'];
-        // document.getElementById(`backlogMail${i}`).innerHTML = users[i]['email'];
+        document.getElementById(`userImage${i}`).src = users[i]['profil_image'];
+        document.getElementById(`backlogUser${i}`).innerHTML = users[i]['first_name'] + ' ' + users[i]['last_name'];
+        document.getElementById(`backlogMail${i}`).innerHTML = users[i]['email'];
         document.getElementById(`backlogCategory${i}`).innerHTML = tasks[i]['category'];
         document.getElementById(`backlogDate${i}`).innerHTML = tasks[i]['date'];
         document.getElementById(`backlogTitle${i}`).innerHTML = tasks[i]['title'];
 
     }
 }
+
+
+
 
 /**
  * This function generate a new backlog
@@ -28,13 +32,13 @@ function updateBacklog() {
 function generateBacklog(i) {
     return `
         <div class="backlog ${tasks[i]['urgency']}">
-            <div class="backlogprofil">
+            <div class="backlogprofil" id="backlogProfil">
                 <img id="userImage${i}" src="img/user-2-64.png" alt="Profilbild">
                 <div class="backloguser">
                     <span class="backlogusername" id="backlogUser${i}">
-                        Hans Peters
+                        Bing Xia
                     </span>
-                    <a class="email" href="mailto:user email" id="backlogMail${i}">hans77@join.de</a>
+                    <a id="backlogMail${i}" class="email" href="mailto:user email">xia.bing@live.com</a>
                 </div>
             </div>
             <span class="backlogcategory" id="backlogCategory${i}">Design</span>
