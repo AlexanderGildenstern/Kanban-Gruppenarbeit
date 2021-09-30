a8a73de306ec59b5901106fcbb06241b04a722c3
NiklasZeiler <zeiler.niklas@yahoo.de>
2021-09-30 18:27:54 +0200

    delete list

--- js/backlog.js
+++ js/backlog.js
@@ -19,7 +19,7 @@ function updateBacklog() {
             document.getElementById(`userImage${i}`).src = tasks[i]["assignedTo"][0]["profil_image"];
         }
         for (let j = 0; j < tasks[i]["assignedTo"]["length"]; j++) {
-            document.getElementById(`backlogUser${i}`).innerHTML += `<li>` + tasks[i]["assignedTo"][j]["first_name"] //+ `<br>`
+            document.getElementById(`backlogUser${i}`).innerHTML += tasks[i]["assignedTo"][j]["first_name"] //+ `<br>`
         }
         document.getElementById(`backlogCategory${i}`).innerHTML = tasks[i]['category'];
         document.getElementById(`backlogDate${i}`).innerHTML = tasks[i]['date'];
